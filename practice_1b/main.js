47c9a82aba117bfccc674be8c6389f1d34acd70c
jensoncrew <jen24078@byui.edu>
2026-01-13 20:29:07 -0700

    added new assignments

---
+++ practice_1b/main.js
@@ -0,0 +1,22 @@
+const PI = 3.14;
+let radius = 3;
+
+console.log("PI:", PI);
+console.log("Radius:", radius);
+
+const one = 1;
+const two = "2";
+
+console.log("one + two =", one + two);
+
+let course = "CSE131";
+
+if (true) {
+	let student = "John";
+	console.log(course);
+	console.log(student);
+}
+
+console.log(course);
+
+// console.log(student);
